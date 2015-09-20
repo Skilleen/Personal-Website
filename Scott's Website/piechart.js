47cce2cafd321abbccfb1f7cec9bd12852922323
Scott Killeen <scott.killeen@hotmail.com>
2015-09-20 16:01:31 -0400

    Updating Javascript chart

--- Scott's Website/piechart.js
+++ Scott's Website/piechart.js
@@ -24,14 +24,15 @@ window.onload = function () {
       indexLabelPlacement: "inside", 
       toolTipContent: "{name}",
       showInLegend: true,
-      indexLabel: "#percent%", 
+      indexLabel: "#percent%",
       dataPoints: [
-        {  y: 7.5, name: "Android Dev", legendMarkerType: "circle"},
-        {  y: 25, name: "Web Dev", legendMarkerType: "circle"},
-        {  y: 7.5, name: "Math Proofs", legendMarkerType: "circle"},
+        {  y: 12, name: "Android Dev", legendMarkerType: "circle"},
+        {  y: 15, name: "Web Dev", legendMarkerType: "circle"},
+        {  y: 10, name: "Automated Scripting", legendMarkerType: "circle"},
         {  y: 15, name: "Shell Scripting", legendMarkerType: "circle"},
-        {  y: 25, name: "Java", legendMarkerType: "circle"},
-        {  y: 10, name: "Python", legendMarkerType: "circle"},
+        {  y: 20, name: "Java", legendMarkerType: "circle"},
+        {  y: 11, name: "Python", legendMarkerType: "circle"},
+        {  y: 12, name: "C#", legendMarkerType: "circle"},
         {  y: 13.5, name: "C", legendMarkerType: "circle"}
       ]
     }
